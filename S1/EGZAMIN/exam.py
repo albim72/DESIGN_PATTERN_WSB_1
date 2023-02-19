4cf2a098ec80f4286d286cc57343c91d8081fc5f
albim72 <marcin.albiniak72@gmail.com>
2023-02-19 14:40:43 +0100

    Update exam.py

--- S1/EGZAMIN/exam.py
+++ S1/EGZAMIN/exam.py
@@ -2,19 +2,19 @@ class Exam:
     def __init__(self):
         self._writing_grade = 0
         self._math_grade = 0
-        
+
     @staticmethod
     def _check_grade(value):
         if not (0 <= value <= 100):
             raise ValueError('ocena musi być wartością z przedziału 0-100')
-        
+
     @property
     def writing_grade(self):
         return self._writing_grade
-    
+
     @writing_grade.setter
     def writing_grade(self,value):
-        self._check_grade()
+        self._check_grade(value)
         self._writing_grade = value
 
     @property
@@ -23,6 +23,5 @@ class Exam:
 
     @math_grade.setter
     def math_grade(self, value):
-        self._check_grade()
+        self._check_grade(value)
         self._math_grade = value
-        
