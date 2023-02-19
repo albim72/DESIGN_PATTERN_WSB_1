1ca4aca5ad35713537117e333d3fa9846197ba3e
albim72 <marcin.albiniak72@gmail.com>
2023-02-19 14:51:52 +0100

    Update grade.py

--- S1/EGZAMIN/grade.py
+++ S1/EGZAMIN/grade.py
@@ -1,12 +1,14 @@
 class Grade:
-    def __get__(self, instance, owner):
+
+    def __init__(self):
         self._value = 0
+    def __get__(self, instance, owner):
+        return self._value
     def __set__(self, instance, value):
         if not (0 <= value <= 100):
             raise ValueError('ocena musi być wartością z przedziału 0-100')
         self._value = value
-        
+
 class ExamD:
     math_grade = Grade()
-    wariting_grade = Grade()
-    
+    wraiting_grade = Grade()
