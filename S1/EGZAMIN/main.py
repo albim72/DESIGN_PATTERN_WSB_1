be092713c1132fa431cdb57f63d5c132a556f67f
albim72 <marcin.albiniak72@gmail.com>
2023-02-19 14:54:48 +0100

    Update main.py

--- S1/EGZAMIN/main.py
+++ S1/EGZAMIN/main.py
@@ -1,5 +1,6 @@
 from Homework import Homework
 from exam import Exam
+from grade import ExamD
 
 
 print("__________HOMEWORK___________")
@@ -19,3 +20,24 @@ assert st2.math_grade == 67
 
 print(st2.writing_grade)
 print(st2.math_grade)
+
+print("__________GRADE___________")
+
+st3_first = ExamD()
+st3_first.math_grade = 78
+st3_first.wraiting_grade = 89
+
+print(f'ocena math: {st3_first.wraiting_grade}')
+print(f'ocena math: {st3_first.math_grade}')
+
+st3_sec = ExamD()
+st3_sec.math_grade = 90
+st3_sec.wraiting_grade = 98
+
+print(f'ocena math: {st3_sec.wraiting_grade}')
+print(f'ocena math: {st3_sec.math_grade}')
+print("jeszcze raz pierwszy")
+print(f'ocena math: {st3_first.wraiting_grade}')
+print(f'ocena math: {st3_first.math_grade}')
+
+
