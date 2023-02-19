11df58ac64d40287d048ccbad9d3e63f539edd8e
albim72 <marcin.albiniak72@gmail.com>
2023-02-19 14:41:34 +0100

    Update main.py

--- S1/EGZAMIN/main.py
+++ S1/EGZAMIN/main.py
@@ -1,6 +1,21 @@
 from Homework import Homework
+from exam import Exam
 
+
+print("__________HOMEWORK___________")
 st = Homework()
 st.grade = 86
 assert st.grade == 86
 print(st.grade)
+
+print("__________EXAM___________")
+
+st2 = Exam()
+st2.writing_grade = 78
+st2.math_grade = 67
+
+assert st2.writing_grade == 78
+assert st2.math_grade == 67
+
+print(st2.writing_grade)
+print(st2.math_grade)
